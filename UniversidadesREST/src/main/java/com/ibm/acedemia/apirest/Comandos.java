b32ec3f5ad095a7e5b2d46e3de84680c8fffc5fd
Erick Martinez <erick_martinez9101@outlook.com>
2022-05-05 17:00:59 -0500

    verificando la conexion y probando el CRUD

--- UniversidadesREST/src/main/java/com/ibm/acedemia/apirest/Comandos.java
+++ UniversidadesREST/src/main/java/com/ibm/acedemia/apirest/Comandos.java
@@ -1,10 +1,11 @@
 package com.ibm.acedemia.apirest;
-
 import com.ibm.academia.apirest.UniversidadesREST.entities.Carrera;
 import com.ibm.academia.apirest.UniversidadesREST.services.CarreraDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class Comandos implements CommandLineRunner
@@ -16,12 +17,20 @@ public class Comandos implements CommandLineRunner
     @Override
     public void run(String... args) throws Exception {
 
-       /* Carrera finanzas = new Carrera();
-        finanzas.setNombre("Finanzas II");
+        Carrera carrera= null;
 
-        Carrera carreraGuardada= carreraDao.guardar(finanzas);
+        Optional <Carrera> oCarrerra = CarreraDAO.buscarPorId(2);
+        if (oCarrerra.isPresent()){
+            carrera = oCarrerra.get();
+            System.out.println(carrera.toString());
+        }else{
+            System.out.println("Carrera no encontrada");
 
-        System.out.println(carreraGuardada.toString());*/
+        }
+
+        carrera.setCantidadAnios(7);
+        carrera.setCantidadMaterias(66);
+        carreraDao.guardar(carrera);
 
     }
 }
